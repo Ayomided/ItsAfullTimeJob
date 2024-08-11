41f4316228fee9a9d05e8568a7aa5d8fe7e5065a
Ayomided <adediji.d@yahoo.com>
2024-08-11 21:42:00 +0100

    feat: create navigation signpost

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Ayomided/ItsAfullTimeJob.git/components"
+	"github.com/a-h/templ"
+)
+
+// //go:embed static/*
+// var static embed.FS
+
+func main() {
+	homepage := components.Index()
+	formPage := components.Form()
+	counter := components.Counter()
+
+	router := http.NewServeMux()
+
+	router.Handle("/", templ.Handler(homepage))
+	router.Handle("/form", templ.Handler(formPage))
+	router.Handle("/counter", templ.Handler(counter))
+
+	log.Fatalln(http.ListenAndServe(":3000", router))
+}
